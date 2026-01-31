b373b0a13c5c12eb4e24a37b22beef105d9105df
j-jagger <joejaggermain@outlook.com>
2026-01-31 01:31:34 +0000

    Added more comments, Implemented UAC elevation. Formalized some language.

--- disklarity.py
+++ disklarity.py
@@ -1,18 +1,45 @@
 """
 Disklarity
 
+Entry Point
+
 Disk Checking tool, designed to be a 'modern' replacement for Gibson Research's ValiDrive.
 At the time of writing this, ValiDrive was last updated 689.80 days ago.
+
 """
 import sys
 from wmi import WMI
 from lib.helpers import *
 from lib.gui import StartInterface
 from lib.mainfunctions import VerifyDrive, ListDevices,PrintHelp
+import ctypes
+
+AMOUNT_OF_CHECKS = 10
+
+
+
+def CheckElevation(argv):
+    try:
+        s32 = ctypes.windll.shell32 # WinShell32 Object
+        if not s32.IsUserAnAdmin():
+            print("[!] Disklarity requires Administrative Privileges to open Win32 devices as file-like objects.")
+            print("[!] Attempting UAC Elevation...")
+        else:
+            return
+
+        argument_line = " ".join(str(arg) for arg in argv) # Reconstruct arguments as string
+        executable = str(sys.executable) # Reconstruct executable location
+        ret = s32.ShellExecuteW(None, "runas", executable, argument_line, None, 1) # Re-execute as administrator.
+        if ret <= 32:
+            raise RuntimeError(f"[!] UAC Elevation Failed! RET: {ret}")
+        sys.exit(0)
+    except Exception as e:
+        print(f"[!] Elevation failed! {e}")
 
 
 def Entrypoint():
-    args = sys.argv
+    args = sys.argv # Arguments
+
 
     try:
         args[1]
@@ -20,6 +47,8 @@ def Entrypoint():
         print("Please provide arguments. Run 'help' for more info.")
         exit(1)
     
+    CheckElevation(args) # Checks for elevation and elevates if possible.
+
     if args[1] == "list":
         ListDevices()
         exit()
@@ -33,7 +62,7 @@ def Entrypoint():
         exit()
 
     else:
-        VerifyDrive(args[1],10)
+        VerifyDrive(args[1],AMOUNT_OF_CHECKS)
 
 
 if __name__ == "__main__":
--- lib/mainfunctions.py
+++ lib/mainfunctions.py
@@ -9,59 +9,68 @@ c = WMI()
 def VerifyDrive(path,x_many_reads):
 
     try:
-        for i in c.Win32_DiskDrive():
-            dev = wmi_to_dict(i)
+        for i in c.Win32_DiskDrive(): # Irritating to use WMI object
+            dev = wmi_to_dict(i) # Very nice to use dict object
             if dev.get("DeviceID") == path:
-                wmidevice = dev
+                wmidevice = dev # Ensuring passed device actually exists.
         if not wmidevice:
-            raise FileNotFoundError()
+            raise FileNotFoundError() 
     except Exception as e:
         print(f"[!] Failed to load {path} via WMI: {e}")
         exit()
 
     print(f"[✔] Located device {wmidevice.get("Caption")} [Path @ {path}]")
 
-    chunksize = wmidevice.get("DefaultBlockSize") or wmidevice.get("BytesPerSector")
-    filesystemsize = int(wmidevice.get("Size"))
+    chunksize = wmidevice.get("DefaultBlockSize") or wmidevice.get("BytesPerSector") # Might remove BytesPerSector.
+    try:
+        filesystemsize = int(wmidevice.get("Size")) # get FS size so we can divide it in a moment.
+    except TypeError as e:
+        print(f"[!] Fatal error getting FileSystem size. If {path} is an adapter (SD MMC, Etc), ensure the storage device is inserted.")
+        print(f"[?] Debug: {e}")
+        exit(1)
 
-    step = filesystemsize // x_many_reads
+    step = filesystemsize // x_many_reads # how many steps in bytes should we do
 
 
 
     print(
-        f"[?] Device tells us it's ~{int(filesystemsize)/1e9:.2f}GB. We'll be the judge of that."
+        f"[?] Device tells us it's ~{int(filesystemsize)/1e9:.2f}GB."
     )
 
     print(
-        f"[✔] Step set to {step}. Ready for liftoff, houston. Hit enter to continue."
+        f"[✔] Step set to {step}. Ready for liftoff. Hit enter to continue."
     )
 
-    input()
+    input() # Will stop yielding when user hits enter in stdin.
 
-    with open(path, "rb") as device:
-        for chunk in range(x_many_reads):
-            offset = step * chunk
+    with open(path, "rb") as device: # Opens device as a file-like object. This would be so much easier in UNIX/BSD.
+        for chunk in range(x_many_reads): # For every chunk in the amount of reads we should do
+            offset = step * chunk 
 
-            device.seek(offset)
-            print(f"Trying read... [Offset: {offset}]")
+            device.seek(offset) # Change Pointer location
+            print(f"Trying read... [Offset: {offset}]") 
             try:
-                data = device.read(chunksize)
+                data = device.read(chunksize) # Attempt read
                 if not data:
                     print(f"!!! EMPTY READ @ {offset} !!!")
-                elif data == bytes(chunksize):
+                elif data == bytes(chunksize): # Might modify this, it's a bit inefficient I believe as it allocates an entire bytes object. If chunksize is like 1gb (not sure why anyone would set that in the first place) this would be a major issue.
                     print(f"!!! READ EMPTY BYTES @ {offset} !!!")
                 else:
-                    print(f"[✔] Chunk @ {offset} normal. SHA256[16]: {hashlib.sha256(data).hexdigest()[:16]}")
+                    # TODO: Add SHA logs and a filesystem verification system.
+                    print(f"[✔] Chunk @ {offset} normal. SHA256 Trunc. 16: {hashlib.sha256(data).hexdigest()[:16]}")
             except:
                 print(f"!!! READ FAILED @ {offset} !!!")
 
 def ListDevices():
-    print("Listing Devices...")
+    print("[?] Listing Devices...")
 
-    for i in c.Win32_DiskDrive():
-        dev = wmi_to_dict(i)
+    try:
+        for i in c.Win32_DiskDrive(): # Another annoying WMI device
+            dev = wmi_to_dict(i) # Another beautiful, charming dict-man.
 
-        print(f"Found: '{dev.get('Model')}' @ {dev.get('DeviceID')}'")
+            print(f"[✔] Found: '{dev.get('Model')}' @ {dev.get('DeviceID')}'") # Verbosity!
+    except Exception as e:
+        print(f"[!] Failed to list devices: {e}")
 
 def PrintHelp():
     # I would use one multiline print statement for this, but it looks weird.
@@ -72,7 +81,7 @@ def PrintHelp():
     print("| Command & Name        | Description                      |")
     print("+-----------------------+----------------------------------+")
     print("| disklarity.py list    | Lists all Win32_DiskDrives.      |")
-    print("| disklarity.py \\.\dev0 | Runs Disklarity on that drive.   |")
+    print("| disklarity.py dev0    | Runs Disklarity on that drive.   |")
     #print("| disklarity.py gui     | Opens the User Interface.        |") not yet implemented
     print("+-----------------------+----------------------------------+")
     print("\n" * 3)
